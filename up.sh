c726827f3f23cac9dcfb16d99602dfd2b4ded6e8
Shinji Kirino <shinjikirino@gmail.com>
2018-05-01 00:14:24 +0000

    volume mount setting added, and bug fix.

--- up.sh
+++ up.sh
@@ -2,4 +2,4 @@
 
 docker-compose up -d
 sleep 30;
-docker run --rm -P -d -p 22:22 --name kippo_kippo_1 --link kippo_db_1:mysql dariusbakunas/kippo
+docker run --rm -P -d -p 22:22 --name dockerkippotoolchain_kippo_1 --link dockerkippotoolchain_db_1:mysql dariusbakunas/kippo
